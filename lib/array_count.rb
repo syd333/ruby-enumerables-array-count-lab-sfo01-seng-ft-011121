929e984669dd34bb0bfe9ecc178b7e9b3d92a539
sydney miranda <sydmiranda@icloud.com>
2021-01-04 21:55:38 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,8 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  index = 0
   array.count do
-    if 
+    if array[index]
 
 end
 
